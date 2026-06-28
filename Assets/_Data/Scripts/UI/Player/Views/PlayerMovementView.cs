fcce6c8cf2f17911a52793af348dd0a8d36874f1
le cuong <lecuong7500@gmail.com>
2026-06-28 22:34:24 +0700

    create_movements_when_using_keyboard

--- Assets/_Data/Scripts/UI/Player/Views/PlayerMovementView.cs
+++ Assets/_Data/Scripts/UI/Player/Views/PlayerMovementView.cs
@@ -3,16 +3,44 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerMovementView : MonoBehaviour, IPointerUpHandler, IDragHandler, IPointerDownHandler
+public class PlayerMovementView : LifetimeScope, IPointerUpHandler, IDragHandler, IPointerDownHandler, IStartInit
 {
+    [SerializeField] private InputManager _inputManager;
     [SerializeField] private RectTransform _container;
     [SerializeField] private RectTransform _joystickBg;
     [SerializeField] private RectTransform _point;
     [SerializeField] private float _radius;
-
+    
     private Vector2 startTouchPos;
     private Vector2 directionTouchPos;
     public Action<Vector2, float> MoveEvent { get; set; }
+
+    protected override void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+         _inputManager.MovePressedEvent += OnKeyboardMove;    
+    }
+
+    private void OnKeyboardMove(Vector2 keyboardDirection)
+    {
+        if (keyboardDirection == Vector2.zero)
+        {
+            _point.anchoredPosition = Vector2.zero;
+            directionTouchPos = Vector2.zero;
+            this.MoveEvent?.Invoke(Vector2.zero, 0f);
+        }
+        else
+        {
+            Vector2 normalizedDir = keyboardDirection.normalized;
+            _point.anchoredPosition = normalizedDir * _radius;
+            directionTouchPos = normalizedDir * _radius;
+            this.MoveEvent?.Invoke(directionTouchPos, 1f);
+        }
+    }
 
     public void OnPointerUp(PointerEventData eventData)
     {
