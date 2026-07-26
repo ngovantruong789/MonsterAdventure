740658ae42861da1c552402d630ec9b09bb82291
le cuong <lecuong7500@gmail.com>
2026-07-27 01:50:48 +0700

    create_item_SO

---
+++ Assets/_Data/Scripts/Items/Enums/EItemType.cs
@@ -0,0 +1,7 @@
+public enum EItemType
+{
+    Medicine = 1,   
+    Pokeball = 2,   
+    KeyItem = 3,    
+    HeldItem = 4,   
+}
---
+++ Assets/_Data/Scripts/ScripableObjects/Items/ItemSO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Objects/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    [Header("1. Thông tin giao diện (UI)")]
+    public string itemName;         
+
+    [TextArea(3, 5)]
+    public string description;      
+    public Sprite itemImage;         
+
+    [Header("2. Định danh & Kinh tế")]
+    public int itemID;              
+    public EItemType itemType;       
+    public int buyPrice;            
+    public int sellPrice;           
+
+    [Header("3. Chỉ số Tác dụng (Effect Payload)")]
+    public float effectValue;       
+    public bool isConsumable;       
+}
